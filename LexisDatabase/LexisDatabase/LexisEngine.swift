550fb764e07965decf8fd60fd714159a0db5e1f2
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-08-27 09:44:51 -0700

    Updates LexisEngine layout

--- LexisDatabase/LexisDatabase/LexisEngine.swift
+++ LexisDatabase/LexisDatabase/LexisEngine.swift
@@ -11,14 +11,47 @@ import Foundation
 //The Lexis Engine is reponsible for reading through the Dictionary text file
 //And extracting meaningful information from it, for the purpose of
 //Storing representing in a LexisWord data structure
-class LexisEngine {
+public class LexisEngine
+{
     
+    public let instance = LexisEngine()
     
-    private struct Regex {
-        
-        static let wordType = "(TRANS|V|N|ADJ|ADV|CONJ)"
-        static let dictionaryCode = "\\[(\\w+)\\]"
+    private init()
+    {
         
     }
     
+    
+    
+}
+
+private extension LexisEngine
+{
+    
+    struct Regex
+    {
+        
+        static let wordType = "(TRANS|V|N|ADJ|ADV|CONJ)"
+        static let dictionaryCode = "\\[(\\w+)\\]"
+        static let definitionPhrase = "(?<=:: )(.*)"
+    }
+    
+}
+
+
+//MARK: Loading the text file
+extension LexisEngine
+{
+    
+    func readTextFile() -> String?
+    {
+        
+        let resourceName = "LatinDictionary"
+        
+        guard let url = Bundle.main.url(forResource: resourceName, withExtension: "txt")
+        else
+        {
+            return nil
+        }
+    }
 }
