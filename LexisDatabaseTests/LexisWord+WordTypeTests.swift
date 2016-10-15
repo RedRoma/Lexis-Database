6629f69c8ae87bb402a26d9c29a6aae222021480
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-10-15 12:34:31 -0700

    Adds Unit Tests

--- LexisDatabaseTests/LexisWord+WordTypeTests.swift
+++ LexisDatabaseTests/LexisWord+WordTypeTests.swift
@@ -70,3 +70,145 @@ class LexisWord_WordTypeTests: LexisTest
 
     
 }
+
+
+//MARK: Enum Tests
+extension LexisWord_WordTypeTests
+{
+    func testConjugationNames()
+    {
+        let conjugation = Generators.randomConjugation
+        let name = conjugation.name
+        
+        let result = Conjugation.from(name: name)
+        
+        XCTAssertFalse(result == nil)
+        XCTAssertTrue(result == conjugation)
+    }
+    
+    func testConjugationNamesWithUnknown()
+    {
+        let randomName = AlchemyGenerator.alphanumericString()
+        let result = Conjugation.from(name: randomName)
+        XCTAssertTrue(result == nil)
+    }
+    
+    func testConjugationShortCodes()
+    {
+        let codes = ["1st", "2nd" , "3rd", "4th"]
+        let code = codes.anyElement!
+        
+        let conjugation = Conjugation.fromShortCode(code: code)
+        XCTAssertTrue(conjugation != .Irregular)
+    }
+  
+    func testConjugationShortCodesWithUnknown()
+    {
+        let randomCode = AlchemyGenerator.alphanumericString()
+        
+        let result = Conjugation.fromShortCode(code: randomCode)
+        XCTAssertTrue(result == .Irregular)
+    }
+    
+    func testVerbTypeNames()
+    {
+        let verbType = Generators.randomVerbType
+        let name = verbType.name
+        
+        let result = VerbType.from(name: name)
+        XCTAssertFalse(result == nil)
+        XCTAssertTrue(result == verbType)
+    }
+    
+    func testVerbTypeNamesWithUnknown()
+    {
+        let randomName = AlchemyGenerator.alphabeticString()
+        let result = VerbType.from(name: randomName)
+        XCTAssertTrue(result == nil)
+    }
+    
+    func testGenderNames()
+    {
+        let gender = Generators.randomGender
+        let name = gender.name
+        
+        let result = Gender.from(name: name)
+        XCTAssertFalse(result == nil)
+        XCTAssertTrue(result == gender)
+    }
+    
+    func testGenderNamesWithUnknown()
+    {
+        let randomName = AlchemyGenerator.alphabeticString()
+        let result = Gender.from(name: randomName)
+        XCTAssertTrue(result == nil)
+    }
+    
+    func testDeclensionName()
+    {
+        let declension = Generators.randomDeclension
+        let name = declension.name
+        
+        let result = Declension.from(name: name)
+        XCTAssertFalse(result == nil)
+        XCTAssertTrue(result == declension)
+    }
+    
+    func testDeclensionNameWithUnknown()
+    {
+        let randomName = AlchemyGenerator.alphabeticString()
+        let result = Declension.from(name: randomName)
+        XCTAssertTrue(result == nil)
+    }
+    
+    func testDeclensionShortForms()
+    {
+        let declension = Generators.randomDeclension
+        let shortForm = declension.shortForm
+        
+        let result = Declension.fromShortCode(code: shortForm)
+        
+        XCTAssertTrue(result == declension)
+    }
+    
+    func testDeclensionShortFormsWithUnknown()
+    {
+        let randomShortForm = AlchemyGenerator.alphabeticString()
+        let result = Declension.fromShortCode(code: randomShortForm)
+        XCTAssertTrue(result == .Undeclined)
+    }
+    
+    func testCaseTypeNames()
+    {
+        let caseType = Generators.randomCaseType
+        let name = caseType.name
+        
+        let result = CaseType.from(name: name)
+        XCTAssertFalse(result == nil)
+        XCTAssertTrue(result == caseType)
+    }
+    
+    func testCaseTypeNamesWithUnknown()
+    {
+        let randomName = AlchemyGenerator.alphanumericString()
+        let result = CaseType.from(name: randomName)
+        XCTAssertTrue(result == nil)
+    }
+    
+    func testCaseTypeCodes()
+    {
+        let caseType = Generators.randomCaseType
+        let codes = [ "NOM", "GEN", "ACC", "DAT", "ABL", "VOC", "LOC" ]
+        let code = codes.anyElement!
+        
+        let result = CaseType.from(shortCode: code)
+        XCTAssertTrue(result == caseType)
+    }
+    
+    func testCaseTypeCodesWithUnknown()
+    {
+        let randomCode = AlchemyGenerator.alphabeticString()
+        let result = CaseType.from(shortCode: randomCode)
+        XCTAssertTrue(result == .Unknown)
+    }
+}
