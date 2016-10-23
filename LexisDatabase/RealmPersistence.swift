5920fed5888e7e07316a5a60b8a7514a2ca1e26f
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-10-22 20:30:11 -0700

    Adds Realm Data Structures

--- LexisDatabase/RealmPersistence.swift
+++ LexisDatabase/RealmPersistence.swift
@@ -64,7 +64,83 @@ class RealmPersistence: LexisPersistence
     
 }
 
-fileprivate class RealmWord: Object
+class RealmForm: Object
 {
+    dynamic var form = ""
+    
+    static func from(form: String) -> RealmForm
+    {
+        let realmForm = RealmForm()
+        realmForm.form = form
+        return realmForm
+    }
+    
+    var asString: String { return form }
+}
+
+
+class RealmDefinitionTerm: Object
+{
+    dynamic var term = ""
+    
+    static func from(term: String) -> RealmDefinitionTerm
+    {
+        let instance = RealmDefinitionTerm()
+        instance.term = term
+    
+        return instance
+    }
+    
+    var asString: String { return term }
+}
+
+class RealmDefinition: Object
+{
+    var terms = List<RealmDefinitionTerm>()
+    
+    static func from(lexisDefinition: LexisDefinition) -> RealmDefinition
+    {
+        
+        let terms = lexisDefinition.terms.map(RealmDefinitionTerm.from)
+        
+        let definition = RealmDefinition()
+        definition.terms = List(terms)
+        
+        return definition
+    }
+    
+    var asLexisDefinition: LexisDefinition
+    {
+        let terms: [String] = self.terms.map() { $0.asString }
+        
+        return LexisDefinition(terms: terms)
+    }
+}
+
+class RealmWord: Object
+{
+    var forms = List<RealmForm>()
+    
+    var definitions = List<RealmDefinition>()
+    
+    dynamic var wordType = ""
+    
+    static func from(lexisWord: LexisWord) -> RealmWord?
+    {
+        let forms = lexisWord.forms.map(RealmForm.from)
+        
+        let definitions = lexisWord.definitions.map(RealmDefinition.from)
+        
+        guard let wordType = lexisWord.wordType.asJSONString(serializer: BasicJSONSerializer.instance) else {
+            return nil
+        }
+    
+        let realmWord = RealmWord()
+        realmWord.forms = List(forms)
+        realmWord.definitions = List(definitions)
+        realmWord.wordType = wordType
+        
+        return realmWord
+    }
     
 }
