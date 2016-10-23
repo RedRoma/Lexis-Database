66005bf9ca5af94ea097c9d36af75204da908472
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-10-22 19:23:42 -0700

    Adds Realm Persistence

---
+++ LexisDatabase/RealmPersistence.swift
@@ -0,0 +1,68 @@
+//
+//  RealmPersistence.swift
+//  LexisDatabase
+//
+//  Created by Wellington Moreno on 10/22/16.
+//  Copyright © 2016 RedRoma, Inc. All rights reserved.
+//
+
+import Archeota
+import Foundation
+import RealmSwift
+
+
+class RealmPersistence: LexisPersistence
+{
+
+    fileprivate let realm: Realm
+    
+    init?()
+    {
+        do
+        {
+            try self.realm = Realm()
+        }
+        catch
+        {
+            LOG.error("Failed to load Realm: \(error)")
+            return nil
+        }
+    }
+    
+    
+    func save(words: [LexisWord]) throws
+    {
+        
+    }
+    
+    func getAllWords() -> [LexisWord]
+    {
+        return []
+    }
+    
+    func removeAll()
+    {
+        
+    }
+    
+    func remove(word: LexisWord)
+    {
+        
+    }
+    
+    func searchForWords(inWordList terms: String) -> [LexisWord]
+    {
+        return []
+    }
+    
+    func searchForWords(inDefinition terms: String) -> [LexisWord]
+    {
+        return []
+    }
+    
+}
+
+fileprivate class RealmWord: Object
+{
+    
+}
